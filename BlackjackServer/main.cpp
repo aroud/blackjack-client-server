fcf92c783632683d270625f2584ac08c7d3a0fe0
Andrew Roud <aroud.by@gmail.com>
2021-04-11 15:37:31 +0300

    Remove all boost code from project

--- BlackjackServer/main.cpp
+++ BlackjackServer/main.cpp
@@ -8,33 +8,22 @@
 
 int main()
 {
-    //try
-    //{
-    //    // Check command line arguments.
-    //    if (argc != 4)
-    //    {
-    //        std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-    //        std::cerr << "  For IPv4, try:\n";
-    //        std::cerr << "    receiver 0.0.0.0 80 .\n";
-    //        std::cerr << "  For IPv6, try:\n";
-    //        std::cerr << "    receiver 0::0 80 .\n";
-    //        return 1;
-    //    }
+    try
+    {
+        // Initialise the server.
+        http::server::Server s("127.0.0.1", "6789", "");
 
-    //    // Initialise the server.
-    //    http::server::Server s(argv[1], argv[2], argv[3]);
+        // Run the server until stopped.
+        s.Run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "exception: " << e.what() << "\n";
+    }
 
-    //    // Run the server until stopped.
-    //    s.Run();
-    //}
-    //catch (std::exception& e)
-    //{
-    //    std::cerr << "exception: " << e.what() << "\n";
-    //}
-
-    blackjack::Game game(2);
+   /* blackjack::Game game(2);
     game.PlayGame();
-    return 0;
+    return 0;*/
 
     return 0;
 }
